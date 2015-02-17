2b2bafa4e12598da6183e10039ac3e365a1ddd8d
Alvaro Pereyra <alvaro@xendacentral.com>
2015-02-16 20:22:26 -0600

    Updates with fixes for 4.0.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CpDemo
+module App
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -24,6 +24,7 @@ module CpDemo
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
+    config.secret_key_base = "5a2f3526648afb77e04dcb282229af6345712366283cde2bda4e8ad8591b33a71e3831c263dd1c203515f2b4d2f2aa727010d3aaf5306d0cda9abda1dabe44eb"
   end
 end
